1c2d4a2f0a7d111449de952e205c20ab6b39b1c7
tuliang <info@tuliang.org>
2014-04-12 12:28:07 +0800

    add copy paste cut

--- init.el
+++ init.el
@@ -29,4 +29,21 @@
                (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
 (global-set-key (kbd "C-c r") 'rename-this-buffer-and-file)
 
+;; copy paste cut
+(defun pbcopy ()
+  (interactive)
+  (call-process-region (point) (mark) "pbcopy")
+  (setq deactivate-mark t))
 
+(defun pbpaste ()
+  (interactive)
+  (call-process-region (point) (if mark-active (mark) (point)) "pbpaste" t t))
+
+(defun pbcut ()
+  (interactive)
+  (pbcopy)
+  (delete-region (region-beginning) (region-end)))
+
+(global-set-key (kbd "C-c c") 'pbcopy)
+(global-set-key (kbd "C-c v") 'pbpaste)
+(global-set-key (kbd "C-c x") 'pbcut)
